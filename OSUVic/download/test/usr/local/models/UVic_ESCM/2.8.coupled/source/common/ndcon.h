d291b6d72f88361111f82fbc844b71861f24b889
andreasschmittner <aschmitt@coas.oregonstate.edu>
2021-01-13 09:30:32 -0800

    removed /OSUVic

--- OSUVic/download/test/usr/local/models/UVic_ESCM/2.8.coupled/source/common/ndcon.h
+++
@@ -1,9 +0,0 @@
-!====================== include file "ndcon.h" ========================
-
-!     various non dimensional quantities:
-
-!     radian = degrees per radian
-!     pi     = something good to eat
-
-      real radian, pi
-      common /ndcon/ radian, pi
--- OSUVic/download/test/usr/local/models/UVic_ESCM/2.8.coupled/source/mom/cpolar.h
+++
@@ -1,6 +0,0 @@
-!====================== include file "cpolar.h" =========================
-
-!     polar transform coefficients used to transform velocities near
-!     poles before filtering
-
-      common /cpolar/ spsin(imt), spcos(imt)
